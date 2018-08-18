be6ca86c8418585c9e968841a59922acccadbe99
xuhuigithub <1498472791@qq.com>
2018-08-18 14:46:04 +0800

    Update Pipeline.groovy
    helm init 添加了 --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts 参数

--- src/io/estrado/Pipeline.groovy
+++ src/io/estrado/Pipeline.groovy
@@ -18,7 +18,7 @@ def helmLint(String chart_dir) {
 def helmConfig() {
     //setup helm connectivity to Kubernetes API and Tiller
     println "initiliazing helm client"
-    sh "helm init"
+    sh "helm init --stable-repo-url https://kubernetes.oss-cn-hangzhou.aliyuncs.com/charts"
     println "checking client/server version"
     sh "helm version"
 }
